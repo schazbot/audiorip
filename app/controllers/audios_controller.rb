6f7f42f0e67a9a80eee05dcb3dd37a7c87fe0633
schazbot <charlotte.pearce1984@gmail.com>
2019-08-13 17:59:39 +0100

    added to readme

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -2,19 +2,12 @@ require "streamio-ffmpeg"
 
 class AudiosController < ApplicationController
   def index
-    @audios = Audio.all
-  end
-
-  def new
-    # movie = FFMPEG::Movie.new("/Users/cpearce2/Development/code/Mod2/audiorip/app/assets/images/screen.mov")
-    # @audio = movie.transcode("audio_extract.mp3", :ab => 128)
   end
 
   def create
-    # @audio = Audio.new(params['title'], params['artist'], params['url'])
-    # movie = YoutubeDL.download "https://www.youtube.com/watch?v=gvdf5n-zI14", output: 'some_file.mp4'
-    # @audio = movie.transcode( "audio_extract.mp3", :ab => 128 )
     system("youtube-dl -x --audio-format mp3 #{params[:url]}")
+    # system("youtube-dl --extract-audio --audio-format mp3 -o "%(title)s.%(ext)s" #{params[:url]}")
+
   end
 
   def show
