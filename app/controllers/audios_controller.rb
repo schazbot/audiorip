b4d982a0b06584d7d80ce91707e28e1aa40a752d
schazbot <charlotte.pearce1984@gmail.com>
2019-08-13 17:06:13 +0100

    audios controller changes

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,28 +1,23 @@
-require 'streamio-ffmpeg'
+require "streamio-ffmpeg"
 
 class AudiosController < ApplicationController
+  def index
+    @audios = Audio.all
+  end
 
-    def index
-        @audios = Audio.all
-    end
+  def new
+    movie = FFMPEG::Movie.new("/Users/cpearce2/Development/code/Mod2/audiorip/app/assets/images/screen.mov")
+    @audio = movie.transcode("audio_extract.mp3", :ab => 128)
+  end
 
-    def new
-        movie = FFMPEG::Movie.new("/Users/cpearce2/Development/code/Mod2/audiorip/app/assets/images/screen.mov")
-        @audio = movie.transcode( "audio_extract.mp3", :ab => 128 )
-    end
+  def create
+    # @audio = Audio.new(params['title'], params['artist'], params['url'])
+    # movie = YoutubeDL.download "https://www.youtube.com/watch?v=gvdf5n-zI14", output: 'some_file.mp4'
+    # @audio = movie.transcode( "audio_extract.mp3", :ab => 128 )
+    system("youtube-dl -x --audio-format mp3 #{params[url]}")
+  end
 
-    def create
-        # @audio = Audio.new(params['title'], params['artist'], params['url'])
-        # movie = YoutubeDL.download "https://www.youtube.com/watch?v=gvdf5n-zI14", output: 'some_file.mp4'
-        # @audio = movie.transcode( "audio_extract.mp3", :ab => 128 )
-        system("youtube-dl -x --audio-format mp3 #{params[url]}")
-
-    end
-
-    def show
-        @audio = Audio.find(params[:id])
-    end
-
-
-
+  def show
+    @audio = Audio.find(params[:id])
+  end
 end
