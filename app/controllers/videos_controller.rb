6bfd3e5e4f011c5ee0a65061f3c955c362f7075a
schazbot <charlotte.pearce1984@gmail.com>
2019-08-20 18:00:17 +0100

    tidying up video page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,22 +5,10 @@ class VideosController < ApplicationController
   def search
     wrapper = Discogs::Wrapper.new("music search", user_token: "OLOYyBwmpfSDUfWyRGKBRNdRMKVQvKRphmmipHuL")
     results = wrapper.search(params[:query])
-    #  @results = []
-    #  results.values[1].each { |result| @results << result }
-     first_result = results.values[1][0]
-     
-     
-     # release_id = results.values[1][0][:"id"]
-     # release_title = results.values[1][0][:"title"]
-     # release = wrapper.get_release(release_id)
-     
-     resource_id = results[:results][1][:id]
-     
-    #  binding.pry
-    # resource_link = wrapper.search(params[:query])[:results][0][:resource_url]
+    
+    resource_id = results[:results][1][:id]
+    @release_title = results[:results][1][:title]
     @video_url = wrapper.get_release(resource_id)[:videos][0][:uri]
-    
-    
 
     render :index
   end
