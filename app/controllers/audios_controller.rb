bad082e4c1581ffd4136ee18cfd95179c953f7f8
schazbot <charlotte.pearce1984@gmail.com>
2019-08-20 14:59:44 +0100

    renddering text onto view page

--- app/controllers/audios_controller.rb
+++ app/controllers/audios_controller.rb
@@ -1,8 +1,5 @@
 require "streamio-ffmpeg"
 class AudiosController < ApplicationController
-  def index
-  end
-
   def create
     system("youtube-dl -x --audio-format mp3 #{params[:url]}")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-  get '/', to: 'audios#index'
-  get '/new', to: 'audios#new'
+  get '/', to: 'audios#new'
   post '/audio', to: 'audios#create'
   post '/search', to: 'videos#search'
 
+  get '/videos', to: 'videos#index'
 
 end
